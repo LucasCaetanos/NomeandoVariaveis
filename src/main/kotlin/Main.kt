1b9da30d875fce64b7f784ea082e759b870aa14c
LucasCaetanos <87659302+LucasCaetanos@users.noreply.github.com>
2021-10-25 11:51:34 -0300

    Exercicio para nomear variaveis

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    var nome = ""
+    var idade = 0
+    var emprego = ""
+    var nacionalidade = ""
+    var sangue = ""
+    var ano = 0
+
+    println("Digite seu nome: ")
+    nome = readLine()!!
+    println("Digite a sua idade: ")
+    idade = readLine()!!.toInt()
+    println("Digite qual profissão deseja seguir: ")
+    emprego= readLine()!!
+    println("Digite a sua nacionalidade: ")
+    nacionalidade= readLine()!!
+    println("Digite o seu tipo sanguineo: ")
+    sangue= readLine()!!
+
+    ano = 2021 - idade
+
+    println("Seu nome é $nome, tem $idade anos de idade, quer ser $emprego, sua nacionalidade, é $nacionalidade" +
+            " Seu tipo sanguineo é $sangue e você nasceu no ano de $ano")
+}
